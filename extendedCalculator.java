552bd10f7d80b79188abe36438c55c832faf3c26
gnana jyothi <01fe22bca239@kletech.ac.in>
2024-03-30 19:34:20 +0530

    Adding Extendedcalculator

---
+++ extendedCalculator.java
@@ -0,0 +1,11 @@
+public class extendedCalculator {
+
+    float square(float operand_1){
+        return (operand_1*operand_1);
+    }
+    float cube(float operand_1){
+        return (operand_1*operand_1*operand_1);
+}
+}
+
+
